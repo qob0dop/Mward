e5209972ac628f834791b9aa295bb2fbecacd36a
qob0dop <1069804648@qq.com>
2025-07-11 11:32:34 +0800

    init

---
+++ js/lay-module/appconfig/appconfig.js
@@ -0,0 +1,15 @@
+/**
+  扩展一个http模块
+**/
+
+layui.define(function (exports) { //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
+    var obj = {
+        api: "http://10.102.38.186:5000",  //根据自己项目的端口而定
+        twd_webpage: "http://10.102.38.186:8801",
+        subsys_id:"zy_wpws2"
+    };
+
+    //输出test接口
+    exports('appconfig', obj);
+});
+
