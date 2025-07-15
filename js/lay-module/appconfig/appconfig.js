1b6ef4b5ad56162b5fec6259c306cd21520f6646
qob0dop <1069804648@qq.com>
2025-07-15 08:09:10 +0800

    头部标题

--- js/lay-module/appconfig/appconfig.js
+++ js/lay-module/appconfig/appconfig.js
@@ -4,7 +4,7 @@
 
 layui.define(function (exports) { //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
     var obj = {
-        api: "http://10.102.38.186:5000",  //根据自己项目的端口而定
+        api: "http://10.102.41.142:5105",  //根据自己项目的端口而定
         twd_webpage: "http://10.102.38.186:8801",
         subsys_id:"zy_wpws2"
     };
