880d38cce0346e4491e94d8c2d7310f1c77aeb2a
qob0dop <1069804648@qq.com>
2025-07-16 14:08:45 +0800

    列表高亮

--- js/lay-module/appconfig/appconfig.js
+++ js/lay-module/appconfig/appconfig.js
@@ -2,14 +2,14 @@
   扩展一个http模块
 **/
 
-layui.define(function (exports) { //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
-    var obj = {
-        api: "http://10.102.41.142:5105",  //根据自己项目的端口而定
-        twd_webpage: "http://10.102.38.186:8801",
-        subsys_id:"zy_wpws2"
-    };
+layui.define(function (exports) {
+  //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
+  var obj = {
+    api: "http://10.102.41.142:5105", //根据自己项目的端口而定
+    twd_webpage: "http://10.102.38.186:8801",
+    subsys_id: "zy_wpws2",
+  };
 
-    //输出test接口
-    exports('appconfig', obj);
+  //输出test接口
+  exports("appconfig", obj);
 });
-
