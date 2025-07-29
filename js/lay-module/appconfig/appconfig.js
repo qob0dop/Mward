dd603c5aa1d067d2c1ad12fbfd1d239a6b73c95e
qob0dop <1069804648@qq.com>
2025-07-29 15:34:41 +0800

    api修改+patient页面css整理

--- js/lay-module/appconfig/appconfig.js
+++ js/lay-module/appconfig/appconfig.js
@@ -5,7 +5,8 @@
 layui.define(function (exports) {
   //提示：模块也可以依赖其它模块，如：layui.define('layer', callback);
   var obj = {
-    api: "http://10.102.41.142:5105", //根据自己项目的端口而定
+    // api: "http://10.102.41.142:5105", //根据自己项目的端口而定
+    api: "http://10.102.41.142:5251", //根据自己项目的端口而定
     twd_webpage: "http://10.102.38.186:8801",
     subsys_id: "zy_wpws2",
   };
