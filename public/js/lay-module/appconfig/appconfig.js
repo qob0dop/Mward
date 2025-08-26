8a2adbeae3f2dbe704a2b3546dd9db5fa162092d
qob0dop <1069804648@qq.com>
2025-08-26 16:26:57 +0800

    8/26

--- public/js/lay-module/appconfig/appconfig.js
+++ public/js/lay-module/appconfig/appconfig.js
@@ -7,7 +7,7 @@ layui.define(function (exports) {
 
   // 检查localStorage中是否有保存的API地址
   var savedApiUrl = localStorage.getItem("api_url");
-  var defaultApi = "http://10.102.41.142:5250"; // 默认API地址
+  var defaultApi = "http://10.102.41.142:5251"; // 默认API地址
 
   var obj = {
     // api: "http://10.102.41.142:5105", //根据自己项目的端口而定
